67997c6f57fbc16f05a31234e01bec660ef05686
Elvis020 <elvis.amoako@stu.ucc.edu.gh>
2020-11-07 13:21:29 +0000

    Added a few animations

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Header() {
     return (
         <div className="header">
-            <h1 style={{padding: '30px',}}>Expense Tracker</h1>
+            <h1 style={{padding: '30px'}}>Expense Tracker</h1>
         </div>
     )
 }
